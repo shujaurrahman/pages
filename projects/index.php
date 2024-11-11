e6e64e42371be39736d506587e46e5d9d26b8789
shujaurrahman <Shujaurrehman210@gmail.com>
2024-11-12 02:51:16 +0530

    component

---
+++ projects/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projects</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div class="mx-auto max-w-[750px] px-4 pb-20 pt-6 text-lg sm:px-12 md:px-16">
+        <div class="mt-8 sm:mt-16">
+            <h1 class="text-xl font-semibold text-zinc-100 sm:text-2xl">Projects</h1>
+            <p class="mt-2 text-base sm:mt-7 sm:text-lg">
+                I have worked on a variety of projects over the years; some as a hobby, some as a proof of concept, and others to solve my own pain points. Here are some of the projects that I have worked on.
+            </p>
+        </div>
+
+        <div class="mt-4 flex flex-col gap-5 sm:mt-7">
+            <div class="relative overflow-hidden rounded-lg bg-zinc-800 p-3">
+                <h2 class="flex items-center justify-between">
+                    <div class="flex flex-col items-start gap-1 sm:flex-row sm:items-center sm:gap-2">
+                        <a href="https://roadmap.sh" class="text-lg font-medium text-zinc-50 sm:text-xl" target="_blank">roadmap.sh</a>
+                        <button class="flex items-center gap-1 rounded-3xl px-2 py-0.5 text-xs transition-colors bg-yellow-400 text-black hover:bg-yellow-600">
+                            acquired
+                        </button>
+                    </div>
+                </h2>
+                <p class="my-3 text-sm sm:text-base pr-0 sm:pr-14">
+                    Learning paths for different roles and skills. Also features an AI roadmap generator, custom roadmap editor, teams usage, and more.
+                </p>
+
+                <div class="my-5 grid grid-cols-2 gap-2 sm:grid-cols-4">
+                    <div class="flex flex-col rounded-lg bg-gradient-to-b from-zinc-900/40 px-3 py-3">
+                        <span class="mb-1 text-xs uppercase tracking-wide text-zinc-500">Visitors / Mo</span>
+                        <span class="text-zinc-400">+1.3m</span>
+                    </div>
+                    <div class="flex flex-col rounded-lg bg-gradient-to-b from-zinc-900/40 px-3 py-3">
+                        <span class="mb-1 text-xs uppercase tracking-wide text-zinc-500">GitHub Stars</span>
+                        <span class="text-zinc-400">+268k</span>
+                    </div>
+                    <div class="flex flex-col rounded-lg bg-gradient-to-b from-zinc-900/40 px-3 py-3">
+                        <span class="mb-1 text-xs uppercase tracking-wide text-zinc-500">Reg. Accounts</span>
+                        <span class="text-zinc-400">+850k</span>
+                    </div>
+                    <div class="flex flex-col rounded-lg bg-gradient-to-b from-zinc-900/40 px-3 py-3">
+                        <span class="mb-1 text-xs uppercase tracking-wide text-zinc-500">Reg. Teams</span>
+                        <span class="text-zinc-400">+10k</span>
+                    </div>
+                </div>
+
+                <div class="my-1.5 flex flex-wrap gap-1.5">
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">TypeScript</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">React.js</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">Node.js</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">Astro</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">RESTful APIs</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">Tailwind</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">GitHub</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">Git</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">AWS</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">Terraform</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">Ansible</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">Docker</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">CI/CD</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">SEO</span>
+                    <span class="rounded-full border border-zinc-700 px-2 text-sm text-zinc-400">Content Writing</span>
+                </div>
+
+                <div class="-mx-3 -mb-3 -mt-3 flex items-center justify-between p-3">
+                    <button class="flex items-center gap-1 rounded-lg bg-zinc-700 py-2 pl-2 pr-2 text-sm hover:text-zinc-300 sm:py-1 sm:pr-3 hover:bg-zinc-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down">
+                            <path d="m6 9 6 6 6-6"></path>
+                        </svg>
+                        <span class="hidden sm:inline-block">Highlights</span>
+                    </button>
+
+                    <div class="flex items-center gap-2 text-zinc-500">
+                        <a href="https://roadmap.sh" target="_blank" class="flex items-center gap-1.5 rounded-lg py-1 text-base transition-colors hover:text-zinc-300 sm:text-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe h-4 w-4 sm:h-4 sm:w-4">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path>
+                                <path d="M2 12h20"></path>
+                            </svg>
+                            <span class="hidden sm:inline-block">Website</span>
+                        </a>
+                        <a href="https://github.com/kamranahmedse/developer-roadmap" target="_blank" class="flex items-center gap-1.5 rounded-lg px-2.5 py-1 text-base transition-colors hover:text-zinc-300 sm:text-sm">
+                        <svg
+                         width="24" height="24" viewBox="0 0 24 24" focusable="false"
+                        class="h-4 w-4 sm:h-4 sm:w-4" aria-hidden="true">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M12 0.300003C5.4 0.300003 0 5.7 0 12.3C0 17.6 3.4 22.1 8.2 23.7C8.8 23.8 9 23.4 9 23.1C9 22.8 9 22.1 9 21.1C5.7 21.8 5 19.5 5 19.5C4.5 18.1 3.7 17.7 3.7 17.7C2.5 17 3.7 17 3.7 17C4.9 17.1 5.5 18.2 5.5 18.2C6.6 20 8.3 19.5 9 19.2C9.1 18.4 9.4 17.9 9.8 17.6C7.1 17.3 4.3 16.3 4.3 11.7C4.3 10.4 4.8 9.3 5.5 8.5C5.5 8.1 5 6.9 5.7 5.3C5.7 5.3 6.7 5 9 6.5C10 6.2 11 6.1 12 6.1C13 6.1 14 6.2 15 6.5C17.3 4.9 18.3 5.3 18.3 5.3C19 7 18.5 8.2 18.4 8.5C19.2 9.3 19.6 10.4 19.6 11.7C19.6 16.3 16.8 17.3 14.1 17.6C14.5 18 14.9 18.7 14.9 19.8C14.9 21.4 14.9 22.7 14.9 23.1C14.9 23.4 15.1 23.8 15.7 23.7C20.5 22.1 23.9 17.6 23.9 12.3C24 5.7 18.6 0.300003 12 0.300003Z"
+                        fill="currentColor"></path>
+                        </svg>
+                            <span class="hidden sm:inline-block">GitHub</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
